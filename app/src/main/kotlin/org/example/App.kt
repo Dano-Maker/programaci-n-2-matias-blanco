84709549f5a1f1f9672473d87d0284806809cced
Negroooh <p.carreras@alumno.etec.um.edu.ar>
2025-05-30 09:38:26 -0300

    Para carpeta vacia

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,11 @@
+package org.example
+
+fun main() {
+    val complejo = Complejo()
+    complejo=Complejo()
+    println("Objeto Complejo creado")
+    var x:Int=1
+    x:Int=2
+}
+
+println("Hola mundo")
---
+++ app/src/main/kotlin/org/example/completo.kt
@@ -0,0 +1,5 @@
+package org.example
+
+class Complejo {
+    // Por ahora la clase no tiene atributos
+}
