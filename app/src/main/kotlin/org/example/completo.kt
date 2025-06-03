c65479549167e78bd44256c2928c7266732025ce
Negroooh <p.carreras@alumno.etec.um.edu.ar>
2025-06-03 08:30:43 -0300

    borrado de datos

--- app/src/main/kotlin/org/example/completo.kt
+++ app/src/main/kotlin/org/example/completo.kt
@@ -1,7 +1,11 @@
 package org.example
 
 class Complejo(val real: Double = 0.0, val imaginario: Double = 0.0) {
-    fun mostrarMensaje() {
-        println("¡Hola! Soy un número complejo con parte real $real y parte imaginaria $imaginario")
+    override fun toString(): String {
+        return if (imaginario >= 0) {
+            "$real + ${imaginario}i"
+        } else {
+            "$real - ${-imaginario}i"
+        }
     }
 }
